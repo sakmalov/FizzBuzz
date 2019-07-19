15e44a9a49f851eca2ecef59e87537064cc4cd45
Guy Calloway <[guyfcalloway@gmail.com]>
2019-07-19 13:44:16 +0100

    New Tests added to spec file, Fizzbuzz condition completed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,14 @@
-def fizzbuzz
+
+def fizzbuzz(number)
+  if number <= 0
+    "Not a positive integer"
+  elsif number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    "#{number}"
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,31 @@
-
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed 13' do
+    expect(fizzbuzz(13)).to eq '13'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "positive whole numbers only supported" when passed 0' do
+    expect(fizzbuzz(0)).to eq 'positive whole numbers only supported'
+  end
+
+  it 'returns "2" when passed 5' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(7)).to eq '7'
   end
 end
